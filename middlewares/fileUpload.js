b3c9e40555431739569d97499d910055d61e42ec
Ashil Thomas <ashilthomas31@gmail.com>
2024-01-29 01:17:06 +0530

    static file change

--- middlewares/fileUpload.js
+++ middlewares/fileUpload.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 const storage = multer.diskStorage({
     destination:function(req,file,cb,){
-        cb(null, './upload/restaurantPhotographs'); 
+        cb(null, '../../upload/restaurantPhotographs'); 
     },
     filename:function(req,file,cb,){
         cb(null,Date.now()+ path.extname(file.originalname))
