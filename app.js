62bb72b07c781d71e83981d37b6e7321048f7feb
Ashil Thomas <ashilthomas31@gmail.com>
2024-01-29 01:56:53 +0530

    intial commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const userRouters = require('./userRouters')
 const cors = require("cors")
 const cookieParser = require("cookie-parser")
 const restauratRouters = require("./restaurantRouters")
-
+const path = require('path')
 app.use(cors({
   credentials:true,
   origin:true
@@ -13,7 +13,8 @@ app.use(cors({
 app.use(express.static('upload'))
 app.use(cookieParser())
 app.use(express.json())
-app.use('/upload',express.static(__dirname +'/upload'))
+// app.use(__dirname + '/upload',express.static(__dirname +'/upload'))
+app.use('/upload', express.static(path.join(__dirname, 'upload')));
 app.use(express.urlencoded({ extended: true }));
 
 
